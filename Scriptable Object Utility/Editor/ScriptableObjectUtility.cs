9b9fe868660483a384d901e34727d6eabf2d4135
Philippe Da Silva <philippe.dasilva@indiefreaks.com>
2014-01-20 22:53:59 +0100

    Update ScriptableObjectUtility.cs
    I found useful to return the created asset on this method as well as providing one that will open a SaveAs dialog window to name the file upfront.

--- Scriptable Object Utility/Editor/ScriptableObjectUtility.cs
+++ Scriptable Object Utility/Editor/ScriptableObjectUtility.cs
@@ -4,7 +4,7 @@ using System.IO;
 
 public static class ScriptableObjectUtility
 {
-    public static void CreateAsset<T>() where T : ScriptableObject
+    public static T CreateAsset<T>() where T : ScriptableObject
     {
         T asset = ScriptableObject.CreateInstance<T>();
 
@@ -25,5 +25,38 @@ public static class ScriptableObjectUtility
         AssetDatabase.SaveAssets();
         EditorUtility.FocusProjectWindow();
         Selection.activeObject = asset;
+        
+        return asset;
+    }
+    
+    /// <summary>
+    ///	This makes it easy to create, name and place unique new ScriptableObject asset files.
+    /// </summary>
+    public static T CreateAssetWithSavePanel<T>() where T : ScriptableObject
+    {
+        T asset = ScriptableObject.CreateInstance<T>();
+
+        string path = AssetDatabase.GetAssetPath(Selection.activeObject);
+        if (path == "")
+        {
+            path = "Assets";
+        }
+        else if (Path.GetExtension(path) != "")
+        {
+            path = path.Replace(Path.GetFileName(AssetDatabase.GetAssetPath(Selection.activeObject)), "");
+        }
+
+        //string assetPathAndName = AssetDatabase.GenerateUniqueAssetPath(path + "/New " + typeof(T).ToString() + ".asset");
+        string assetPathAndName = EditorUtility.SaveFilePanelInProject("Create " + typeof(T).Name, "", "asset", "Please, set the name of your asset", path);
+        if (!string.IsNullOrEmpty(assetPathAndName))
+        {
+            AssetDatabase.CreateAsset(asset, assetPathAndName);
+            AssetDatabase.SaveAssets();
+            Selection.activeObject = asset;
+        }
+
+        EditorUtility.FocusProjectWindow();
+
+        return asset;
     }
 }
